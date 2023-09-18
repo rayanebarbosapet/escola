d6083e941ceb1a2dc8aed9b70d181425edadddc5
Silvia Fernanda Pereira <ART0060843@aluno.firjansenaisesi.com.br>
2023-09-18 10:51:14 -0300

    enums

---
+++ src/main/java/enums/Uf.java
@@ -0,0 +1,15 @@
+package enums;
+
+public enum Uf {
+    //Norte
+    AC, AM, AP, PA, RO, RR, TO,
+    //Nordeste
+    AL, BA, CE, MA, PB, PE, PI, RN, SE,
+    //Centro-Oeste
+    DF, GO, MS, MT,
+    //Sudeste
+    ES, MG, RJ, SP,
+    //Sul
+    PR, RS, SC
+
+}
